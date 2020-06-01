7f4f79d83029fbb8400b1472fb755b2844f4873f
gsstferreira <gustavo.shuiti@gmail.com>
2020-06-01 02:24:50 -0300

    Adicionar arquivos de projeto.

---
+++ MineWorld/overviewerConfig.js
@@ -0,0 +1,71 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "icons/marker_home.png",
+            "spawnMarker2x": "icons/marker_home_2x.png",
+            "queryMarker": "icons/marker_location.png",
+            "queryMarker2x": "icons/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "world"
+    ],
+    "map": {
+        "debug": true,
+        "cacheTag": "1590986533",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "Overviewer Render (lighting)",
+            "zoomLevels": 10,
+            "defaultZoom": 1,
+            "maxZoom": 10,
+            "path": "world-lighting",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "world",
+            "last_rendertime": 1590981832,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                546,
+                99,
+                1580
+            ],
+            "minZoom": 0,
+            "spawn": [
+                546,
+                99,
+                1580
+            ],
+            "north_direction": 0
+        }
+    ]
+};
